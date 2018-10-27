69cbc37dc464fc9673276913255432f5cc54f9ec
DeybisMelendez <pixelweb2007@gmail.com>
2018-10-27 13:22:18 -0600

    menu agregado

--- scripts/HUD2.gd
+++ scripts/HUD2.gd
@@ -1,6 +1,17 @@
 extends HBoxContainer
 
+func _ready():
+	global.time = 300
+
 func _physics_process(delta):
 	$p1.set_text("P1 Left: " + str(global.vidas_j1))
 	$p2.set_text("P2 Left: " + str(global.vidas_j2))
 	$time.set_text("T: " + str(global.time))
+
+func _on_Timer_timeout():
+	if global.time > 0:
+		global.time -= 1
+	else: #Death Match
+		global.vidas_j1 = 0
+		global.vidas_j2 = 0
+		$Timer.stop()
