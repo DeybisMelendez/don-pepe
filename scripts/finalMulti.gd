7e5f1c190694b0e43aa35728b257cbb186cb82b8
DeybisMelendez <pixelweb2007@gmail.com>
2018-10-31 11:05:55 -0600

    sonidos arreglados

--- scripts/finalMulti.gd
+++ scripts/finalMulti.gd
@@ -5,5 +5,6 @@ var menu = load ("res://escenas/menu.tscn")
 func _ready():
 	$Label.set_text("The winner is Player " + str(global.ganador))
 	$AudioStreamPlayer.play()
-	yield($AudioStreamPlayer,"finished")
+
+func _on_Timer_timeout():
 	get_tree().change_scene_to(menu)
