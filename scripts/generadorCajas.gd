e126a9c098f9502afb673078c27e9a754ac0176d
Matías Muñoz Espinoza <writkas2@gmail.com>
2018-10-31 13:53:25 -0300

    no mas precionar el boton y generar cajas

--- scripts/generadorCajas.gd
+++ scripts/generadorCajas.gd
@@ -29,5 +29,8 @@ func _ready():
 				add_child(nE)
 
 func _input(event):
-	if Input.is_action_just_pressed("ui_accept"):
-		get_tree().reload_current_scene()
+	# no más !
+#	if Input.is_action_just_pressed("ui_accept"):
+#		get_tree().reload_current_scene()
+		
+	pass
